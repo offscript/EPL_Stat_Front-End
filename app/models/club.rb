ab79236ee1012253f17f9ea5d16b88fdcc716490
Thomas Keating <twkeating1@gmail.com>
2017-10-10 22:06:04 -0400

    Front End update

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,9 +2,10 @@ class Club
   include Mongoid::Document
   store_in collection: "club_collection"
 
-  field :club_name, type: String
   field :season, type: Hash
+  field :club_name, type: String
   field :club_id, type: Integer
+  field :club_logo_url, type: String
   field :primary_color, type: String
   field :secondary_color, type: String
 end
